aaec705a152cfc000a1b8b07716d0a23d2a0d7a4
SGKespace <55636018+SGKespace@users.noreply.github.com>
2023-03-26 15:18:42 +0400

    Используйте шорткат get_object_or_404

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from .models import Pokemon, PokemonEntity
 from django.utils.timezone import localtime
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -57,11 +58,8 @@ def show_pokemon(request, pokemon_id):
     pokemons = Pokemon.objects.all()
 
     for pokemon in pokemons:
-        if pokemon.id == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+        requested_pokemon = get_object_or_404(Pokemon, id=pokemon_id)
+
 
     pokemon_info = {
         "pokemon_id": requested_pokemon.id,
